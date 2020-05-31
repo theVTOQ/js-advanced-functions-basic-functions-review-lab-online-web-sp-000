8940b9916cc8ba2a4fa1ad077c8a293d22274179
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-31 20:43:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function saturdayFun(activity="roller-skate"){
 function mondayWork(activity="go to the office"){
   return `This Monday, I will ${activity}.`;
 }
+
+function wrapAdjective(){
+  return function(adj = "special"){
+    return `You are ${adj}.`;
+  }
+}
