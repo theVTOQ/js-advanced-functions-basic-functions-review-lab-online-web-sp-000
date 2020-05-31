702899352d0784e7e3fcc03793176ee3ee7c2ffe
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-31 20:48:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,18 @@ function wrapAdjective(flair = "*"){
 
 const Calculator = {
   function add(num1, num2){
-    return num1 + num2
+    return num1 + num2;
   }
 
   function subtract(num1, num2){
-    return num1 - num2
+    return num1 - num2;
+  }
+
+  function multiply(num1, num2){
+    return num1 * num2;
+  }
+
+  function divide(num1, num2){
+    return num1 / num2;
   }
 }
